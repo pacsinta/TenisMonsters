8a723c80b54e8e65a26e70af214eda7c1102dcf3
Patrik Csikós <patrik.csikos@proton.me>
2024-03-16 16:03:44 +0100

    Rename to GameController, add endGame and endTurn functions

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -5,22 +5,25 @@ using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
-public class GameRules : NetworkBehaviour
+public class GameController : NetworkBehaviour
 {
     public TextMeshProUGUI debugText;
     public TextMeshProUGUI scoreText;
     public NetworkObject playerObject;
 
+    public uint MaxGameTime = 60 * 5;
+    public uint MaxScore = 5;
     public Vector3 PlayerStartPosition;
     public float ZOffsetOfNet;
 
     private PlayerInfo hostPlayerInfo;
     private PlayerInfo clientPlayerInfo;
+    private uint time = 0;
     public override void OnNetworkSpawn()
     {
         hostPlayerInfo = new PlayerInfo();
 
-        if (!IsHost) return;
+        if (!IsServer) return;
         var clients = NetworkManager.Singleton.ConnectedClientsList;
 
         foreach (var client in clients)
@@ -33,11 +36,45 @@ public class GameRules : NetworkBehaviour
                 spawnRotation = Quaternion.Euler(0, 180, 0);
             }
             NetworkManager.Singleton.SpawnManager.InstantiateAndSpawn(playerObject, client.ClientId, false, true, false, spawnPosition, spawnRotation);
+            clientPlayerInfo = new PlayerInfo(client.ClientId.ToString());
         }
     }
 
-    public void EndGame()
+    private void Start()
     {
-        
+        if(!IsServer && !IsClient && !IsHost)
+        {
+            Debug.LogError("Netcode is not initialized");
+            SceneLoader.LoadScene(SceneLoader.Scene.MenuScene);
+        }
+    }
+
+    private void Update()
+    {
+        time += (uint)Time.deltaTime;
+        debugText.text = time.ToString();
+        /*if (time >= MaxGameTime || hostPlayerInfo.Score >= MaxScore || clientPlayerInfo.Score >= MaxScore)
+        {
+            EndGame();
+        }*/
+    }
+
+    public void EndTurn(bool hostWon)
+    {
+        if (hostWon)
+        {
+            hostPlayerInfo.Score++;
+        }
+        else
+        {
+            clientPlayerInfo.Score++;
+        }
+        scoreText.text = "Host: " + hostPlayerInfo.Score + " Client: " + clientPlayerInfo.Score;
+    }
+
+    private void EndGame()
+    {
+        // TODO: Implement
+        Debug.Log("Game Over");
     }
 }
