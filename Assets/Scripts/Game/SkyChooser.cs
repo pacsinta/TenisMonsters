83de2ac72e6eeb1fb62d295022e90f1c50aac030
Patrik Csikós <patrik.csikos@proton.me>
2024-04-18 13:01:12 +0200

    Create skyChooser script

---
+++ Assets/Scripts/Game/SkyChooser.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public enum SkyType
+{
+    Sunny = 0,
+    Cloudy = 1,
+    CleanNight = 2,
+    CosmicNight = 3,
+    RedSun = 4
+}
+public class SkyChooser : NetworkBehaviour
+{
+    NetworkVariable<SkyType> skyType = new NetworkVariable<SkyType>(SkyType.Sunny);
+
+    public List<Material> skyMaterials;
+
+    public override void OnNetworkSpawn()
+    {
+        if(IsHost)
+        {
+            GameInfo gameInfo = new GameInfo();
+            skyType.Value = (SkyType)PlayerPrefs.GetInt("skyType", 0);
+        }
+        var skyMaterial = skyMaterials[(int)skyType.Value];
+        RenderSettings.skybox = skyMaterial;
+    }
+}
