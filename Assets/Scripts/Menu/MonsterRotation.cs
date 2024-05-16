7fa5bfd3567984a086a11d0883136351795b68e9
Patrik Csikós <patrik.csikos@proton.me>
2024-05-16 14:41:26 +0200

    Rotate the gameObject instead of a public variable

--- Assets/Scripts/Menu/MonsterRotation.cs
+++ Assets/Scripts/Menu/MonsterRotation.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class MonsterRotation : MonoBehaviour
 {
-    public GameObject monster;
     public float rotationSpeed = 10.0f;
     public float rotationAngleLimit = 15.0f;
 
@@ -11,16 +10,16 @@ public class MonsterRotation : MonoBehaviour
     {
         if (rotateRight)
         {
-            monster.transform.Rotate(rotationSpeed * Time.deltaTime * Vector3.up);
-            if (monster.transform.rotation.eulerAngles.y - 180 >= rotationAngleLimit)
+            gameObject.transform.Rotate(rotationSpeed * Time.deltaTime * Vector3.up);
+            if (gameObject.transform.rotation.eulerAngles.y - 180 > rotationAngleLimit)
             {
                 rotateRight = false;
             }
         }
         else
         {
-            monster.transform.Rotate(rotationSpeed * Time.deltaTime * Vector3.down);
-            if (monster.transform.rotation.eulerAngles.y - 180 < -rotationAngleLimit)
+            gameObject.transform.Rotate(rotationSpeed * Time.deltaTime * Vector3.down);
+            if (gameObject.transform.rotation.eulerAngles.y - 180 < -rotationAngleLimit)
             {
                 rotateRight = true;
             }
